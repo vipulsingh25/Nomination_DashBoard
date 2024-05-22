475b09d20bd3ab208cb9c8722046ea18c2dc9711
Vipul Singh <85404156+vipulsingh25@users.noreply.github.com>
2024-05-22 11:31:15 +0530

    Add files via upload

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SDET_Project.Startup))]
+namespace SDET_Project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
